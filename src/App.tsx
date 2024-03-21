3d8faa22a818555ff9c013b117a87a75c24a3bd3
afkdev8 <mr.infinidy@protonmail.com>
2024-03-22 00:19:47 +0100

    Add chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { ChakraProvider } from "@chakra-ui/react"
 
 function App() {
     return (
-        <>
-        
-        </> 
+        <ChakraProvider>
+            <div>
+                <h1>React App</h1>
+            </div>
+        </ChakraProvider>
     );
 }
 
